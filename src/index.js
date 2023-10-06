d22b68aa6fea79071ba0d20c49fe4fb1991dd3ce
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2023-10-06 15:36:14 -0600

    refactor(cli): user flow into separate functions
    Trying to make the code more readable and easier to follow by using
    functions to separate the different parts of the user flow. Taking
    in mind Single Responsibility Principle.

--- src/index.js
+++ src/index.js
@@ -8,64 +8,24 @@ import findOasFromDir from "./services/find-oas-from-dir.js";
 const RC_FILE_NAME = ".apimockrc";
 const TEMP_FOLDER_NAME = ".api-mock-runner";
 
-// TODO: Refactor steps to functions
-// TODO: Add input validation
+/**
+ * Main function to start the mock server
+ * @async
+ * @function main
+ * @returns {Promise<void>}
+ */
 const main = async () => {
   let config;
 
-  const configFileExists = fs.existsSync(`${process.cwd()}/.apimockrc`);
+  const configFileExists = fs.existsSync(`${process.cwd()}/${RC_FILE_NAME}`);
 
   if (configFileExists) {
-    const existingConfig = JSON.parse(
-      fs.readFileSync(`${process.cwd()}/.apimockrc`)
-    );
-    console.table(existingConfig);
-    const useExistingConfig = await confirm({
-      message: "Do you want to use the existing config?",
-    });
-
-    if (useExistingConfig) {
-      config = existingConfig;
-    } else {
-      config = await getInitialValues();
-    }
+    config = await initWithConfigFile();
   } else {
-    config = await getInitialValues();
-    // Create .apimockrc file
-    const filePath = `${process.cwd()}/${RC_FILE_NAME}`;
-    fs.writeFile(filePath, JSON.stringify(config), (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log("Config saved");
-      }
-    });
-    const addRcFileToGitignore = await confirm({
-      message: `Add ${RC_FILE_NAME} to .gitignore?`,
-    });
-    if (addRcFileToGitignore) {
-      addToGitignore(RC_FILE_NAME);
-    }
+    config = await initNoConfigFile();
   }
 
-  /*
-   * NOTE: Regex explanation
-   * - /^(git@|https:\/\/)/: This part of the regex specifies that the string must start with either "git@" or "https://".
-   * - [^\s]+: This part ensures that there is at least one or more characters after "git@" or "https://". It matches any character except whitespace.
-   * - (\.git)$: The regex ends with "\.git", ensuring that the string must end with ".git".
-   */
-  const isOriginRemoteRegex = /^(git@|https:\/\/)[^\s]+(\.git)$/;
-
-  const isOriginRemote = isOriginRemoteRegex.test(config.schemasOrigin);
-
-  if (isOriginRemote) {
-    await cloneGitRepository(config.schemasOrigin);
-    addToGitignore(TEMP_FOLDER_NAME);
-  }
-
-  const schemasDir = isOriginRemote ? TEMP_FOLDER_NAME : config.schemasOrigin;
-
-  const schemas = await findOasFromDir(schemasDir);
+  const schemas = await getSchemas(config.schemasOrigin);
 
   // TODO: change to checkboxes when multiple schemas are supported
   const selectedSchema = await select({
@@ -75,18 +35,114 @@ const main = async () => {
     }),
   });
 
+  await startMockServer(config.initialPort, selectedSchema);
+};
+main();
+
+/**
+ * User flow when the config file already exists
+ * @async
+ * @function initWithConfigFile
+ * @returns {Promise<Config>} A object with the initial values from the user
+ */
+async function initWithConfigFile() {
+  let config;
+  const existingConfig = JSON.parse(
+    fs.readFileSync(`${process.cwd()}/.apimockrc`)
+  );
+  console.table(existingConfig);
+  const useExistingConfig = await confirm({
+    message: "Do you want to use the existing config?",
+  });
+
+  if (useExistingConfig) {
+    config = existingConfig;
+  } else {
+    config = await getInitialValues();
+  }
+  return config;
+}
+
+/**
+ * User flow when the config file doesn't exist
+ * @async
+ * @function initNoConfigFile
+ * @returns {Promise<Config>} A object with the initial values from the user
+ */
+async function initNoConfigFile() {
+  const config = await getInitialValues();
+  // Create .apimockrc file
+  const filePath = `${process.cwd()}/${RC_FILE_NAME}`;
+  fs.writeFile(filePath, JSON.stringify(config), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Config saved");
+    }
+  });
+  const addRcFileToGitignore = await confirm({
+    message: `Add ${RC_FILE_NAME} to .gitignore?`,
+  });
+  if (addRcFileToGitignore) {
+    addToGitignore(RC_FILE_NAME);
+  }
+  return config;
+}
+
+/**
+ * Get the schemas from the origin
+ * @async
+ * @function getSchemas
+ * @param {string} origin - The origin of the schemas (local or remote)
+ * @returns {Promise<Array>} An array of schemas
+ */
+async function getSchemas(origin) {
+  const isOriginRemote = verifyRemoteOrigin(origin);
+
+  if (isOriginRemote) {
+    await cloneGitRepository(origin);
+    addToGitignore(TEMP_FOLDER_NAME);
+  }
+
+  const schemasDir = isOriginRemote ? TEMP_FOLDER_NAME : origin;
+
+  const schemas = await findOasFromDir(schemasDir);
+  return schemas;
+}
+
+/**
+ * Start the mock server
+ * @async
+ * @function startMockServer
+ * @param {number} port - The port to start the mock server
+ * @param {string} schema - The schema to mock
+ * @returns {Promise<void>}
+ */
+async function startMockServer(port, schema) {
   const openApiMocker = new OpenApiMocker({
-    port: config.initialPort,
-    schema: selectedSchema,
+    port: port,
+    schema: schema,
     watch: true,
   });
 
   await openApiMocker.validate();
 
   await openApiMocker.mock();
-};
+}
 
+/**
+ * @typedef {Object} Config
+ * @property {string} schemasOrigin - The origin of the schemas (local or remote)
+ * @property {number} initialPort - The initial port to start the mock server
+ */
+
+/**
+ * get initial values from user
+ * @async
+ * @returns {Promise<Config>} A object with the initial values from the user
+ */
 async function getInitialValues() {
+  // TODO: Add input validation
   const schemasOrigin = await input({
     message: "Enter the repo url or relative path",
   });
@@ -102,19 +158,39 @@ async function getInitialValues() {
   return config;
 }
 
+/**
+ * Append text to .gitignore
+ * @async
+ * @function addToGitignore
+ * @param {string} textToAppend - The text to append to .gitignore
+ * @returns {Promise<void>}
+ */
 async function addToGitignore(textToAppend) {
   // TODO: create function that validates if is already in gitignore
-  fs.appendFile(
-    `${process.cwd()}/.gitignore`,
-    `\n${textToAppend}/$}`,
-    (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`${textToAppend} added to .gitignore`);
-      }
+  fs.appendFile(`${process.cwd()}/.gitignore`, `\n${textToAppend}`, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${textToAppend} added to .gitignore`);
     }
-  );
+  });
 }
 
-main();
+/**
+ * Verify if the origin is remote
+ * @function verifyRemoteOrigin
+ * @param {string} origin - The origin to verify
+ * @returns {boolean} True if the origin is remote, false otherwise
+ */
+function verifyRemoteOrigin(origin) {
+  /*
+   * NOTE: Regex explanation
+   * - /^(git@|https:\/\/)/: This part of the regex specifies that the string must start with either "git@" or "https://".
+   * - [^\s]+: This part ensures that there is at least one or more characters after "git@" or "https://". It matches any character except whitespace.
+   * - (\.git)$: The regex ends with "\.git", ensuring that the string must end with ".git".
+   */
+  const isOriginRemoteRegex = /^(git@|https:\/\/)[^\s]+(\.git)$/;
+
+  const isOriginRemote = isOriginRemoteRegex.test(origin);
+  return isOriginRemote;
+}
