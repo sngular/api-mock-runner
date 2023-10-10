24d92562c35812e4d00cae8bd0bd49f936f49f2e
Adan Toscano <adantoscano@gmail.com>
2023-10-10 17:49:52 +0100

    feat: add function to search

---
+++ src/services/check-string-in-file.js
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+export default async function(stringToCheck, filePath) {
+  const reader = readline.createInterface({ input: fs.createReadStream(filePath) });
+  let exists = false;
+
+  for await (const line of reader) {
+    console.log(line);
+    if(line===stringToCheck) {
+      exists = line===stringToCheck
+      break;
+    }
+  }
+
+  return exists;
+}
