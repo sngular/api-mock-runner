85b0129e571065f8f75717c3104323ff2988796e
Javier Sierra Blazquez <javier.sierra@sngular.com>
2023-10-18 15:17:49 +0000

    refactor: improve clone-git-repository code
    - Add folder as parameter to achieve file reusability
    - Add JSDoc comment in public function
    - Remove printDirectoryContent. No longer needed.
    
    test(clone-git-repository): add unit tests
    Also modifies child_process import, allowing stub its functions with Sinon (https://sinonjs.org/how-to/stub-dependency/)
    
    chore: add c8 library for testing coverage
    
    Closes #18

--- src/services/clone-git-repository.js
+++ src/services/clone-git-repository.js
@@ -1,46 +1,35 @@
-// TODO:
-// - Refactor
-// - console.logs
-// - tests
-// - doc
-
-import { execSync } from 'child_process';
-import * as fs from 'fs';
+import child_process from 'child_process';
+import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import { TEMP_FOLDER_NAME } from './utils.js';
 
 /**
- * Clone a git repository into a temporary folder
- * @param {string} repositoryURL
+ * Clone a git repository
+ * @function cloneGitRepository
+ * @param {string} repositoryURL - The URL of the git repository
+ * @param {string} dirName - The name of the directory where the repository will be cloned
+ * @returns {void}
  */
-async function cloneSchemaRepo(repositoryURL) {
-	resetTempDir();
-	cloneRepository(repositoryURL);
-	printDirectoryContent();
+function cloneGitRepository(repositoryURL, dirName) {
+	resetDirectory(dirName);
+	cloneRepository(repositoryURL, dirName);
 }
 
-function resetTempDir() {
-	removeTempDir();
-	fs.mkdirSync(TEMP_FOLDER_NAME);
+function resetDirectory(dirName) {
+	removeDirectory(dirName);
+	fs.mkdirSync(dirName);
 }
 
-function removeTempDir() {
-	if (fs.existsSync(TEMP_FOLDER_NAME)) {
-		fs.rmSync(TEMP_FOLDER_NAME, { recursive: true });
+function removeDirectory(dirName) {
+	if (fs.existsSync(dirName)) {
+		fs.rmSync(dirName, { recursive: true });
 	}
 }
 
-function cloneRepository(repositoryURL) {
-	execSync(`git clone ${repositoryURL} .`, {
-		cwd: path.resolve(process.cwd(), TEMP_FOLDER_NAME), // path to where you want to save the file
+function cloneRepository(repositoryURL, dirName) {
+	child_process.execSync(`git clone ${repositoryURL} .`, {
+		cwd: path.resolve(process.cwd(), dirName), // path to where you want to save the file
 	});
 }
 
-function printDirectoryContent() {
-	fs.readdirSync(TEMP_FOLDER_NAME).forEach((file) => {
-		console.log(`  - ${file}`);
-	});
-}
-
-export default cloneSchemaRepo;
+export default cloneGitRepository;
--- test/unit/services/clone-git-repository.test.js
+++ test/unit/services/clone-git-repository.test.js
@@ -1,7 +1,49 @@
-import assert from 'assert';
+import { expect, use } from 'chai';
+import child_process from 'child_process';
+import fs from 'fs';
+import { stub } from 'sinon';
+import sinonChai from 'sinon-chai';
+import cloneGitRepository from '../../../src/services/clone-git-repository.js';
 
-describe('Simple unit test', () => {
-	it('should return true', () => {
-		assert.equal(true, true);
+use(sinonChai);
+
+describe('unit:clone-git-repository mocking git clone and fs I/O', () => {
+	const dirName = '';
+	const repositoryURL = '';
+	let execSyncStub;
+	let existsSyncStub;
+	let mkdirSyncStub;
+	let rmSync;
+
+	beforeEach(() => {
+		execSyncStub = stub(child_process, 'execSync');
+		existsSyncStub = stub(fs, 'existsSync');
+		mkdirSyncStub = stub(fs, 'mkdirSync');
+		rmSync = stub(fs, 'rmSync');
+	});
+
+	afterEach(() => {
+		execSyncStub.restore();
+		existsSyncStub.restore();
+		mkdirSyncStub.restore();
+		rmSync.restore();
+	});
+
+	it('should reset temp dir and clone repository', () => {
+		existsSyncStub.returns(true);
+		cloneGitRepository(repositoryURL, dirName);
+		expect(existsSyncStub).to.have.been.called;
+		expect(rmSync).to.have.been.called;
+		expect(mkdirSyncStub).to.have.been.called;
+		expect(execSyncStub).to.have.been.called;
+	});
+
+	it('should create temp dir and clone repository', () => {
+		existsSyncStub.returns(false);
+		cloneGitRepository(repositoryURL, dirName);
+		expect(existsSyncStub).to.have.been.called;
+		expect(rmSync).to.not.have.been.called; // not called
+		expect(mkdirSyncStub).to.have.been.called;
+		expect(execSyncStub).to.have.been.called;
 	});
 });
