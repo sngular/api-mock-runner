82a3684371d148d3561d34bef78f7731f94ad1ce
Javier Sierra <javier.sierra@sngular.com>
2023-12-21 17:09:55 +0100

    refactor: simplify check-string-in-file

--- src/services/check-string-in-file.js
+++ src/services/check-string-in-file.js
@@ -11,16 +11,11 @@ import readline from 'node:readline';
  */
 async function check(stringToCheck, filePath) {
 	const reader = readline.createInterface({ input: fs.createReadStream(filePath) });
-	let exists = false;
-
 	for await (const line of reader) {
 		if (line === stringToCheck) {
-			// REFACTOR: return true instead of setting a variable
-			exists = line === stringToCheck;
-			break;
+			return true;
 		}
 	}
-
-	return exists;
+	return false;
 }
 export const checkStringInFile = { check };
